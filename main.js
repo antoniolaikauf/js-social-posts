5dd2e36374acb3dcdb0863261149bc4a218a4a95
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-12-05 13:58:07 +0100

    add: aggiunti elementi dentro html

--- main.js
+++ main.js
@@ -55,3 +55,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// loop per far mettere card dntro html
+
+posts.forEach((elementi)=>{
+    const {id,content,media,author,likes,created}=elementi
+    const {name,image}=author
+    
+   const card =  `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author"${name}</div>
+                        <div class="post-meta__time">${created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${content}</div>
+            <div class="post__image">
+                <img src="${media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+   `;
+
+    document.getElementById("container").innerHTML+=card;
+})
